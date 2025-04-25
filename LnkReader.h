6509bde0bf5cce9aab64a13606c1b9662a2fb2d1
Bluepuzhong <2929719225@qq.com>
2025-04-25 22:28:20 +0800

    Improve_1

--- LnkReader.h
+++ LnkReader.h
@@ -1,15 +1,11 @@
 
-#include <Windows.h>
-#include <assert.h>
 #include <iostream>
 #include <fstream>
-#include <unordered_map>
+#include <string>
 
-#define         LNK_SEEK_BEG            1
-#define         LNK_SEEK_END            2
-#define         LNK_SEEK_CUR            3
-
-using namespace std;
+#define         LNK_SEEK_BEG            std::ios::beg
+#define         LNK_SEEK_END            std::ios::end
+#define         LNK_SEEK_CUR            std::ios::cur
 
 class LnkReader {
 public:
@@ -40,34 +36,46 @@ public:
     }ITEMID, * LPITEMID;
 
     typedef struct _tagItemType {
-        static const BYTE      ROOT     = 1;
-        static const BYTE      VOLUME   = 2;
-        static const BYTE      FILE     = 3;
+        static const BYTE      ROOT = 1;
+        static const BYTE      VOLUME = 2;
+        static const BYTE      FILE = 3;
     }ITEM_TYPE;
 
 public:
 
     LnkReader() = default;
-
-    int run(string stLnkPath)
+    virtual ~LnkReader()
     {
-        lnkFile.open(stLnkPath, ios::in | ios::binary);     //以二进制只读的方式打开文件流
+        if (lnkFile.is_open())
+        {
+            // 析构函数主要用来关闭文件流
+            lnkFile.close();
+        }
+    }
+
+    std::string get(std::string&& lnkpath)
+    {
+        if (lnkFile.is_open())
+            lnkFile.close();
+
+        lnkFile.open(lnkpath, std::ios::in | std::ios::binary);     //以二进制只读的方式打开文件流
         if (!lnkFile.is_open())
-            return -1;      //打开失败,返回-1
+            return "Failed to open file.";      //打开失败,返回-1
 
         //获取文件大小
         seek(LNK_SEEK_END, 0);  //先跳到结束的位置
         tagFileSize = tell();   //通过tell获取文件大小
         seek(LNK_SEEK_BEG, 0);   //再跳到开头
-
-        Assert(tagFileSize < (LONGLONG)sizeof(LINKFILE_HEADER), "The file is too small.");  //文件大小甚至没有一个lnk文件头大, 很明显文件错误
-
+        //因为lnk文件一般都比较小故可以用以上方法获取文件大小
+        
         //读取4个字节判断是否是lnk类型文件
         read(&lnkHeader.HeaderSize, 4);
-        if (lnkHeader.HeaderSize != 0x4c)//若不是lnk文件则返回 -2
+        if (lnkHeader.HeaderSize != 0x4c)//若不是lnk文件
         {
-            return -2;
+            return "Not lnk file.";
+            lnkFile.close();
         }
+        
         //移动数据到文件开头的位置, 因为读取文件头4个字节时改变了文件读取的位置
         seek(LNK_SEEK_BEG, 0);
 
@@ -75,49 +83,19 @@ public:
         read(&lnkHeader, sizeof(LINKFILE_HEADER));
 
         //获取快捷方式指向的文件路径
-        getLinkTargetIDList();
-
-        //后面的功能就可有可无了
-        ///........
-
-
-        return 0;
+        return getLinkTargetIDList();
     }
-    string getPath() { return tagFilePath; }
 
 private:
-    fstream lnkFile;    //文件流
+    std::fstream lnkFile;    //文件流
     LONGLONG tagFileSize;   //文件大小
     LINKFILE_HEADER lnkHeader;  //lnk文件头
 
-    string tagFilePath;
 
-    //自己写的断言函数
-    void Assert(bool condition, string description)
-    {
-        if (condition)
-        {
-            MessageBoxA(0, description.c_str(), "Error", MB_ICONERROR);
-            throw "assert.";
-        }
-    }
     //跳过数据
-    void seek(DWORD point, int offset)
+    void seek(int pot, int offset)
     {
-        switch (point)
-        {
-        case LNK_SEEK_BEG:        //跳到开头进行偏移
-            lnkFile.seekg(offset, ios::beg);
-            break;
-        case LNK_SEEK_CUR:        //当前位置进行偏移
-            lnkFile.seekg(offset, ios::cur);
-            break;
-        case LNK_SEEK_END:        //结束位置进行偏移
-            lnkFile.seekg(offset, ios::end);
-
-        default:
-            break;
-        }
+        lnkFile.seekg(offset, pot);
     }
     //获取指针当前的位置
     LONGLONG tell()
@@ -138,7 +116,7 @@ private:
         ZeroMemory(pDst, iSize);
     }
     //从数据流中获取一个完整的字符串, 返回字符长度(包括结束符)
-    int getstring(string& src)
+    int getstring(std::string& src)
     {
         src = "";
         int n = 0;
@@ -148,24 +126,21 @@ private:
             if (ch[0] == '\0')
                 break;
 
-            src += string(ch);
+            src += std::string(ch);
         }
 
         return n + 1;
     }
 
-    void getLinkTargetIDList()
+    std::string getLinkTargetIDList()
     {
-        Assert(!(lnkHeader.Flags & 0x1), "The .lnk file doesn't have the HasLinkTargetIDList flag, so we cannot continue.");
 
         int szIDList = 0;
-        char* buf = NULL;
-        string path = "";
+        char* buf = nullptr;
+        std::string path = "";
 
         //读取IDList大小
         read(&szIDList, 2);
-
-        Assert(szIDList == 0, "Failed to read the IDList size."); //读取IDList大小错误或文件错误
 
         for (int szCurrent = 0; szCurrent < szIDList - 2;)
         {
@@ -194,9 +169,8 @@ private:
                 memzero(buf, ItemID.wSize - 3);
 
                 read(buf, ItemID.wSize - 3);
-                Assert(!strcmp(buf, ""), "Failed to read dirver letter.");   //读取盘符失败
 
-                path += string(buf);
+                path += std::string(buf);
 
                 break;
             }
@@ -204,7 +178,7 @@ private:
             {
                 int iFileSize = 0;
                 WORD DosData = 0, DosTime = 0, FileAttributes = 0;
-                string str = "";
+                std::string str = "";
 
                 read(nullptr, 1);  //跳过未知字节
                 /* 注:read函数第一个参数填NULL表示向后跳过 szCount 个字节, 相当于 seek(LNK_SEEK_CUR, szCount) ,可以看看read函数具体细节*/
@@ -217,29 +191,26 @@ private:
                 read(&FileAttributes, 2);           //读取文件(文件夹)类型
                 int reads = getstring(str);         //读取文件名称
 
-                Assert(str == "", "Failed to read file name.");  //读取文件名称失败
-
                 path += str + R"(\)";
 
                 //read(nullptr, ItemID.wSize - 1 - 4 - 2 - 2 - 2 - reads - 3)
                 read(nullptr, ItemID.wSize - reads - 14);   //暂时不需要额外信息
 
                 break;
-            }
-            default:
-                Assert(1, "Failed to read ItemID.");  //ItemID类型未知
-            }
+            }}
 
             szCurrent += ItemID.wSize;
         }
 
-        path = path.substr(0, path.size() - 1); //删去最后一个多余的"\"
-        tagFilePath = path;
+        if (buf != nullptr)
+        {
+            delete[] buf;
+            buf = nullptr;
+        }
 
+        path = path.substr(0, path.size() - 1); //删去最后一个多余的"\"
+        return path;
     }
 
 
 };
-
-
-
--- README.md
+++ README.md
@@ -1,9 +1,4 @@
 # LnkReader
 windows下获取.lnk文件的路径  
 使用方法  
-1.LnkReader类中有两个公开的方法:run和getPath  
-先向run方法里传入指向一个快捷方式文件(.lnk)路径的字符串(例如:C:\Users\User1\Desktop\App1.lnk)  
-2.若run的返回值等于0,则可以使用getPath方法获取lnk的目标文件路径  
-
-run方法返回-1说明文件打开失败  
-返回-2说明不是.lnk文件  
+向get函数传入lnk路径,返回值不是错误信息就是lnk文件指向的路径
